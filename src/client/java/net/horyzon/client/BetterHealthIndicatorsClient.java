74f161a9edeb872bf3652bea16c740281f17e906
horyzonboi <forceishereomgg@gmail.com>
2026-06-06 04:30:13 +0500

    Custom Pipeline Implementation

--- src/client/java/net/horyzon/client/BetterHealthIndicatorsClient.java
+++ src/client/java/net/horyzon/client/BetterHealthIndicatorsClient.java
@@ -3,20 +3,30 @@ package net.horyzon.client;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.rendering.v1.hud.HudElementRegistry;
+import net.fabricmc.fabric.api.client.rendering.v1.level.LevelRenderContext;
+import net.fabricmc.fabric.api.client.rendering.v1.level.LevelRenderEvents;
 import net.horyzon.BetterHealthIndicators;
 
+import net.minecraft.client.renderer.entity.EntityRenderer;
 import net.minecraft.resources.Identifier;
+import net.minecraft.world.entity.player.Player;
+
+import javax.swing.text.html.parser.Entity;
 
 public class BetterHealthIndicatorsClient implements ClientModInitializer {
 
-	@Override
+    @Override
 	public void onInitializeClient() {
-
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+		LevelRenderEvents.END_EXTRACTION.register(CustomRenderPipeline::extractHealth);
+		LevelRenderEvents.AFTER_TRANSLUCENT_TERRAIN.register(CustomRenderPipeline::renderAndDrawHealth);
 
 		HudElementRegistry.addLast(Identifier.fromNamespaceAndPath(BetterHealthIndicators.MOD_ID, "last_element"), HealthHud.HUD_LAYER);
-		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+
 		ClientTickEvents.END_CLIENT_TICK.register(client ->
 				StoreAndPullHealth.getHealth());
+
+
 	}
 
 }
---
+++ src/client/java/net/horyzon/client/CustomRenderPipeline.java
@@ -0,0 +1,214 @@
+package net.horyzon.client;
+
+import com.mojang.blaze3d.buffers.GpuBuffer;
+import com.mojang.blaze3d.buffers.GpuBufferSlice;
+import com.mojang.blaze3d.pipeline.RenderPipeline;
+import com.mojang.blaze3d.systems.CommandEncoder;
+import com.mojang.blaze3d.systems.RenderPass;
+import com.mojang.blaze3d.systems.RenderSystem;
+import com.mojang.blaze3d.vertex.*;
+import net.fabricmc.fabric.api.client.rendering.v1.level.LevelExtractionContext;
+import net.fabricmc.fabric.api.client.rendering.v1.level.LevelRenderContext;
+import net.horyzon.BetterHealthIndicators;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.MappableRingBuffer;
+import net.minecraft.client.renderer.RenderPipelines;
+import net.minecraft.client.renderer.entity.EntityRenderer;
+import net.minecraft.client.renderer.rendertype.RenderType;
+import net.minecraft.resources.Identifier;
+import net.minecraft.world.item.NameTagItem;
+import net.horyzon.client.BetterHealthIndicatorsClient;
+import net.minecraft.world.phys.Vec3;
+import org.joml.Matrix4f;
+import org.joml.Matrix4fc;
+import org.joml.Vector3f;
+import org.joml.Vector4f;
+import org.lwjgl.system.MemoryUtil;
+
+
+import java.util.Optional;
+import java.util.OptionalDouble;
+import java.util.OptionalInt;
+import java.util.UUID;
+
+public class CustomRenderPipeline {
+    //will add actual logic later
+    private record HealthState(int x, int y, int z, float r, float g, float b, float a) {}
+    private static HealthState healthState;
+    private static BufferBuilder buffer;
+    private static MappableRingBuffer vertexBuffer;
+    private static final Vector4f COLOR_MODULATOR = new Vector4f(1f, 1f, 1f, 1f);
+    private static final Vector3f MODEL_OFFSET = new Vector3f();
+    private static final Matrix4f TEXTURE_MATRIX = new Matrix4f();
+
+
+    private static final RenderPipeline HEALTH_PIPELINE = RenderPipelines.register(RenderPipeline.builder(RenderPipelines.DEBUG_FILLED_SNIPPET)
+            .withLocation(Identifier.fromNamespaceAndPath(BetterHealthIndicators.MOD_ID, "pipeline/debug_filled_box_health_display"))
+            .withDepthStencilState(Optional.empty())
+            .build()
+    );
+
+    public static final ByteBufferBuilder ALLOCATOR = new ByteBufferBuilder(RenderType.SMALL_BUFFER_SIZE);
+
+    protected static void extractHealth(LevelExtractionContext context) {
+        healthState = new HealthState(0, 70, 0, 0f, 0f, 1f, 0.5f);
+    }
+
+    protected static void renderAndDrawHealth(LevelRenderContext context) {
+        renderHealth(context);
+        drawFilledThroughWalls(Minecraft.getInstance(), HEALTH_PIPELINE);
+    }
+
+
+    private static void renderHealth(LevelRenderContext context) {
+        PoseStack matrices = context.poseStack();
+        Vec3 camera = context.levelState().cameraRenderState.pos;
+
+        //push
+        matrices.pushPose();
+        matrices.translate(-camera.x, -camera.y, -camera.z);
+        matrices.scale(1f, 1f, 1f);
+
+
+        if (buffer == null) {
+            buffer = new BufferBuilder(ALLOCATOR, HEALTH_PIPELINE.getVertexFormatMode(), HEALTH_PIPELINE.getVertexFormat());
+        }
+        renderFilledBox(matrices.last().pose(), buffer, healthState.x, healthState.y, healthState.z, healthState.x + 1, healthState.y + 1, healthState.z +1, healthState.r, healthState.g, healthState.b, healthState.a);
+        matrices.popPose();
+    }
+
+    private static void renderFilledBox(Matrix4fc positionMatrix, BufferBuilder buffer, float minX, float minY, float minZ, float maxX, float maxY, float maxZ, float red, float green, float blue, float alpha) {
+        // Front Face
+        buffer.addVertex(positionMatrix, minX, minY, maxZ).setColor(red, green, blue, alpha);
+        buffer.addVertex(positionMatrix, maxX, minY, maxZ).setColor(red, green, blue, alpha);
+        buffer.addVertex(positionMatrix, maxX, maxY, maxZ).setColor(red, green, blue, alpha);
+        buffer.addVertex(positionMatrix, minX, maxY, maxZ).setColor(red, green, blue, alpha);
+
+        // Back face
+        buffer.addVertex(positionMatrix, maxX, minY, minZ).setColor(red, green, blue, alpha);
+        buffer.addVertex(positionMatrix, minX, minY, minZ).setColor(red, green, blue, alpha);
+        buffer.addVertex(positionMatrix, minX, maxY, minZ).setColor(red, green, blue, alpha);
+        buffer.addVertex(positionMatrix, maxX, maxY, minZ).setColor(red, green, blue, alpha);
+
+        // Left face
+        buffer.addVertex(positionMatrix, minX, minY, minZ).setColor(red, green, blue, alpha);
+        buffer.addVertex(positionMatrix, minX, minY, maxZ).setColor(red, green, blue, alpha);
+        buffer.addVertex(positionMatrix, minX, maxY, maxZ).setColor(red, green, blue, alpha);
+        buffer.addVertex(positionMatrix, minX, maxY, minZ).setColor(red, green, blue, alpha);
+
+        // Right face
+        buffer.addVertex(positionMatrix, maxX, minY, maxZ).setColor(red, green, blue, alpha);
+        buffer.addVertex(positionMatrix, maxX, minY, minZ).setColor(red, green, blue, alpha);
+        buffer.addVertex(positionMatrix, maxX, maxY, minZ).setColor(red, green, blue, alpha);
+        buffer.addVertex(positionMatrix, maxX, maxY, maxZ).setColor(red, green, blue, alpha);
+
+        // Top face
+        buffer.addVertex(positionMatrix, minX, maxY, maxZ).setColor(red, green, blue, alpha);
+        buffer.addVertex(positionMatrix, maxX, maxY, maxZ).setColor(red, green, blue, alpha);
+        buffer.addVertex(positionMatrix, maxX, maxY, minZ).setColor(red, green, blue, alpha);
+        buffer.addVertex(positionMatrix, minX, maxY, minZ).setColor(red, green, blue, alpha);
+
+        // Bottom face
+        buffer.addVertex(positionMatrix, minX, minY, minZ).setColor(red, green, blue, alpha);
+        buffer.addVertex(positionMatrix, maxX, minY, minZ).setColor(red, green, blue, alpha);
+        buffer.addVertex(positionMatrix, maxX, minY, maxZ).setColor(red, green, blue, alpha);
+        buffer.addVertex(positionMatrix, minX, minY, maxZ).setColor(red, green, blue, alpha);
+    }
+
+
+
+    private static void drawFilledThroughWalls(Minecraft client, @SuppressWarnings("SameParameterValue") RenderPipeline pipeline) {
+        // Build the buffer
+        MeshData builtBuffer = buffer.buildOrThrow();
+        MeshData.DrawState drawParameters = builtBuffer.drawState();
+        VertexFormat format = drawParameters.format();
+
+        GpuBuffer vertices = upload(drawParameters, format, builtBuffer);
+
+        draw(client, pipeline, builtBuffer, drawParameters, vertices, format);
+
+        // Rotate the vertex buffer so we are less likely to use buffers that the GPU is using
+        vertexBuffer.rotate();
+        buffer = null;
+    }
+
+    private static GpuBuffer upload(MeshData.DrawState drawParameters, VertexFormat format, MeshData builtBuffer) {
+        // Calculate the size needed for the vertex buffer
+        int vertexBufferSize = drawParameters.vertexCount() * format.getVertexSize();
+
+        // Initialize or resize the vertex buffer as needed
+        if (vertexBuffer == null ||vertexBuffer.size() < vertexBufferSize) {
+            if (vertexBuffer != null) {
+                vertexBuffer.close();
+            }
+
+           vertexBuffer = new MappableRingBuffer(() -> BetterHealthIndicators.MOD_ID + " health render pipeline", GpuBuffer.USAGE_VERTEX | GpuBuffer.USAGE_MAP_WRITE, vertexBufferSize);
+        }
+
+        // Copy vertex data into the vertex buffer
+        CommandEncoder commandEncoder = RenderSystem.getDevice().createCommandEncoder();
+
+        try (GpuBuffer.MappedView mappedView = commandEncoder.mapBuffer(vertexBuffer.currentBuffer().slice(0, builtBuffer.vertexBuffer().remaining()), false, true)) {
+            MemoryUtil.memCopy(builtBuffer.vertexBuffer(), mappedView.data());
+        }
+
+        return vertexBuffer.currentBuffer();
+    }
+
+    private static void draw(Minecraft client, RenderPipeline pipeline, MeshData builtBuffer, MeshData.DrawState drawParameters, GpuBuffer vertices, VertexFormat format) {
+        GpuBuffer indices;
+        VertexFormat.IndexType indexType;
+
+        if (pipeline.getVertexFormatMode() == VertexFormat.Mode.QUADS) {
+            // Sort the quads if there is translucency
+            builtBuffer.sortQuads(ALLOCATOR, RenderSystem.getProjectionType().vertexSorting());
+            // Upload the index buffer
+            indices = pipeline.getVertexFormat().uploadImmediateIndexBuffer(builtBuffer.indexBuffer());
+            indexType = builtBuffer.drawState().indexType();
+        } else {
+            // Use the general shape index buffer for non-quad draw modes
+            RenderSystem.AutoStorageIndexBuffer shapeIndexBuffer = RenderSystem.getSequentialBuffer(pipeline.getVertexFormatMode());
+            indices = shapeIndexBuffer.getBuffer(drawParameters.indexCount());
+            indexType = shapeIndexBuffer.type();
+        }
+
+        // Actually execute the draw
+        GpuBufferSlice dynamicTransforms = RenderSystem.getDynamicUniforms()
+                .writeTransform(RenderSystem.getModelViewMatrix(), COLOR_MODULATOR, MODEL_OFFSET, TEXTURE_MATRIX);
+        try (RenderPass renderPass = RenderSystem.getDevice()
+                .createCommandEncoder()
+                .createRenderPass(() -> BetterHealthIndicators.MOD_ID + " example render pipeline rendering", client.getMainRenderTarget().getColorTextureView(), OptionalInt.empty(), client.getMainRenderTarget().getDepthTextureView(), OptionalDouble.empty())) {
+            renderPass.setPipeline(pipeline);
+
+            RenderSystem.bindDefaultUniforms(renderPass);
+            renderPass.setUniform("DynamicTransforms", dynamicTransforms);
+
+            // Bind texture if applicable:
+            // Sampler0 is used for texture inputs in vertices
+            // renderPass.bindTexture("Sampler0", textureSetup.texure0(), textureSetup.sampler0());
+
+            renderPass.setVertexBuffer(0, vertices);
+            renderPass.setIndexBuffer(indices, indexType);
+
+            // The base vertex is the starting index when we copied the data into the vertex buffer divided by vertex size
+            //noinspection ConstantValue
+            renderPass.drawIndexed(0 / format.getVertexSize(), 0, drawParameters.indexCount(), 1);
+        }
+
+        builtBuffer.close();
+    }
+    // #endregion custom_pipelines_drawing_phase
+
+    // #region custom_pipelines_clean_up
+    public static void close() {
+        ALLOCATOR.close();
+
+        if (vertexBuffer != null) {
+            vertexBuffer.close();
+            vertexBuffer = null;
+        }
+    }
+
+
+
+}
