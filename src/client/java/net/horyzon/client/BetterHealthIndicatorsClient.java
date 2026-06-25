1f78c58d92348d2274cc34576aafa667c1047440
horyzonboi <forceishereomgg@gmail.com>
2026-06-25 22:38:45 +0500

    Optimized

--- src/client/java/net/horyzon/client/BetterHealthIndicatorsClient.java
+++ src/client/java/net/horyzon/client/BetterHealthIndicatorsClient.java
@@ -3,19 +3,9 @@ package net.horyzon.client;
 
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
-import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
-import net.fabricmc.fabric.api.client.rendering.v1.hud.HudElementRegistry;
-
 import net.fabricmc.fabric.api.client.rendering.v1.level.LevelRenderEvents;
 import net.horyzon.BetterHealthIndicators;
-
-
-import net.horyzon.Networking.GetPlayerHealthPayload;
-import net.minecraft.client.multiplayer.ClientLevel;
 import net.minecraft.resources.Identifier;
-
-import javax.xml.stream.Location;
-
 
 public class BetterHealthIndicatorsClient implements ClientModInitializer {
 	static String basePath = "textures/gui/";
@@ -24,20 +14,9 @@ public class BetterHealthIndicatorsClient implements ClientModInitializer {
 	public static Identifier HEARTS_DEFAULT = Identifier.fromNamespaceAndPath(BetterHealthIndicators.MOD_ID, basePath + "default/hearts_atlas.png");
 
 	public static void createCustomBar(String string, Identifier location) {
-
 	}
-
     @Override
 	public void onInitializeClient() {
-		ClientPlayNetworking.registerGlobalReceiver(GetPlayerHealthPayload.TYPE, ((payload, context) -> {
-			ClientLevel level = context.client().level;
-			if(level == null) {
-				return;
-			}
-			StoreAndPullHealth.playerHealth.put(payload.uuid(), payload.health());
-			System.out.println("REC " + payload.health());
-		}
-		));
 		ClientTickEvents.END_CLIENT_TICK.register(_ -> StoreAndPullHealth.getHealth());
 		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
 		LevelRenderEvents.END_EXTRACTION.register(RenderCustomTexturePipeline::extractHealth);
