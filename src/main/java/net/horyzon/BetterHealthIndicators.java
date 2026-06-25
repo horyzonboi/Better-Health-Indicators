0367e805071ae17a6c42037e6cea49db9aec0e48
horyzonboi <forceishereomgg@gmail.com>
2026-06-25 22:37:06 +0500

    Removed Unnecessary Functionality

--- src/main/java/net/horyzon/BetterHealthIndicators.java
+++ src/main/java/net/horyzon/BetterHealthIndicators.java
@@ -2,14 +2,6 @@ package net.horyzon;
 
 import net.fabricmc.api.ModInitializer;
 
-import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
-import net.fabricmc.fabric.api.networking.v1.PayloadTypeRegistry;
-import net.fabricmc.fabric.api.networking.v1.PlayerLookup;
-import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
-import net.horyzon.Networking.GetPlayerHealthPayload;
-import net.minecraft.network.protocol.common.custom.CustomPacketPayload;
-import net.minecraft.server.level.ServerLevel;
-import net.minecraft.server.level.ServerPlayer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -19,7 +11,5 @@ public class BetterHealthIndicators implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-		PayloadTypeRegistry.clientboundPlay().register(GetPlayerHealthPayload.TYPE, GetPlayerHealthPayload.CODEC);
-
 	}
 }
